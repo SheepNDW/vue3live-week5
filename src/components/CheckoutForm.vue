65366d9a105a448313d31ceed80a8198270e5dbb
SheepNDW <hank3681@gmail.com>
2022-02-15 19:24:37 +0800

    完成表單驗證功能

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -1,51 +1,69 @@
 <template>
-  <form ref="form" class="col-md-6">
+  <Form
+    ref="formRef"
+    class="col-md-6"
+    autocomplete="off"
+    :validation-schema="schema"
+    v-slot="{ errors }"
+    @submit="submitOrder"
+  >
+    {{ errors }}
     <div class="mb-3">
       <label for="email" class="form-label">Email</label>
-      <input
+      <Field
         id="email"
         name="email"
         type="email"
         class="form-control"
+        :class="{ 'is-invalid': errors['email'] }"
         placeholder="請輸入 Email"
+        v-model="form.user.email"
       />
-      <span name="email" class="invalid-feedback"></span>
+      <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
     </div>
 
     <div class="mb-3">
       <label for="name" class="form-label">收件人姓名</label>
-      <input
+      <Field
         id="name"
         name="姓名"
         type="text"
         class="form-control"
+        :class="{ 'is-invalid': errors['姓名'] }"
         placeholder="請輸入姓名"
+        v-model="form.user.name"
       />
-      <span name="姓名" class="invalid-feedback"></span>
+      <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
     </div>
 
     <div class="mb-3">
       <label for="tel" class="form-label">收件人電話</label>
-      <input
+      <Field
         id="tel"
-        name="電話"
-        type="text"
+        name="mobile"
+        type="tel"
         class="form-control"
-        placeholder="請輸入電話"
+        :class="{ 'is-invalid': errors['mobile'] }"
+        placeholder="請輸入手機號碼"
+        v-model="form.user.tel"
       />
-      <span name="電話" class="invalid-feedback"></span>
+      <ErrorMessage name="mobile" v-slot="{ message }">
+        <span role="alert" class="invalid-feedback">{{ message }}</span>
+      </ErrorMessage>
     </div>
 
     <div class="mb-3">
       <label for="address" class="form-label">收件人地址</label>
-      <input
+      <Field
         id="address"
         name="地址"
         type="text"
         class="form-control"
+        :class="{ 'is-invalid': errors['地址'] }"
         placeholder="請輸入地址"
+        v-model="form.user.address"
       />
-      <span name="地址" class="invalid-feedback"></span>
+      <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
     </div>
 
     <div class="mb-3">
@@ -55,18 +73,55 @@
         class="form-control"
         cols="30"
         rows="10"
+        v-model="form.message"
       ></textarea>
     </div>
     <div class="text-end">
       <button type="submit" class="btn btn-danger">送出訂單</button>
     </div>
-  </form>
+  </Form>
 </template>
 
 <script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { reactive, ref } from 'vue';
+import schema from '@/utils/vee-validate-schema';
+import { createOrder } from '@/api/order';
 export default {
   name: 'CheckoutForm',
+  components: { Form, Field, ErrorMessage },
+  setup() {
+    // 表單資料物件
+    const form = reactive({
+      user: {
+        name: '',
+        email: '',
+        tel: '',
+        address: '',
+      },
+      message: '',
+    });
+
+    const mySchema = {
+      姓名: 'required',
+      email: 'required|email',
+      mobile: schema.mobile,
+      地址: 'required',
+    };
+
+    // 送出並生成訂單方法
+    const formRef = ref(null);
+    const submitOrder = async () => {
+      try {
+        const data = await createOrder(form);
+        alert(data.message);
+        formRef.value.resetForm();
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+
+    return { form, schema: mySchema, submitOrder, formRef };
+  },
 };
 </script>
-
-<style lang="scss" scoped></style>
